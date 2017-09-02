82e7d14f9d77d179e51a890110d16d1a5931dde9
Nick Drane <nicholasdrane@gmail.com>
2017-09-02 10:59:27 -0500

    fill screen assembly program added

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,42 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(START)
+@KBD
+D=M
+@BLACK
+D;JNE
+@color
+M=0
+@WRITE
+0;JMP
+(BLACK)
+@color
+M=1
+(WRITE)
+@SCREEN
+D=A
+@pixel
+M=D
+(WRITELOOP)
+@color
+D=M
+@pixel
+A=M
+M=D
+@pixel
+M=M+1
+@24576
+D=A
+@pixel
+D=D-M
+@START
+D;JEQ
+@WRITELOOP
+0;JMP
